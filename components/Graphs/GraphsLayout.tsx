53e4341bfdb26e6358ae97ee22d1c84ebfb99881
Serge <sergesoroka@gmail.com>
2023-01-13 18:44:49 +0200

    Added Visual Button

--- components/Graphs/GraphsLayout.tsx
+++ components/Graphs/GraphsLayout.tsx
@@ -1,18 +1,59 @@
-import React from 'react'
-import TreePlot from './TreePlot'
-import Weekly from './Weekly'
-import Wordcloud from './Wordcloud'
+import { useState } from "react";
+import TreePlot from "./TreePlot";
+import Weekly from "./Weekly";
+import Wordcloud from "./Wordcloud";
 
-import styles from './Graphs.module.css'
+import styles from "./Graphs.module.css";
 
 function GraphsLayout() {
+  const [showGraphs, setShowGraphs] = useState(false);
   return (
-    <div className={styles.graphLayout}>
-        <Weekly />
-        <TreePlot />
-        <Wordcloud />
+    <>
+      <div className={styles.btnWrap}>
+        <svg
+          width="22"
+          height="24"
+          viewBox="0 0 22 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="5.78947" height="24" rx="1" fill="#3F4871" />
+          <rect
+            x="8.10529"
+            y="8.40002"
+            width="5.78947"
+            height="15.6"
+            rx="1"
+            fill="#3F4871"
+          />
+          <rect
+            x="16.2105"
+            y="4.80002"
+            width="5.78947"
+            height="19.2"
+            rx="1"
+            fill="#3F4871"
+          />
+        </svg>
+
+        <button
+          className={styles.btnVisual}
+          onClick={() => setShowGraphs(!showGraphs)}
+        >
+          Інтерактивні
+          <br />
+          візуалізації
+        </button>
       </div>
-  )
+      {showGraphs ? (
+        <div className={styles.graphLayout}>
+          <Weekly />
+          <TreePlot />
+          <Wordcloud />
+        </div>
+      ) : null}
+    </>
+  );
 }
 
-export default GraphsLayout
+export default GraphsLayout;
