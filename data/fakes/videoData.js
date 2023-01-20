a815e19e81212dd110e28fc2e9367060ad576868
Serge <sergesoroka@gmail.com>
2023-01-20 09:13:51 +0200

    Updated Video Data

--- data/fakes/videoData.js
+++ data/fakes/videoData.js
@@ -1,162 +1,102 @@
 export const videoData = [
     {
-        "infographic_id": "1",
-        "infographic_title": "Статистика COVID-19",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/signal-2022-09-26-153245_002.jpeg"
+        "video_id": "1",
+        "video_title": "Фейки вбивають. Випуск №1",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F591777899337391%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "2",
-        "infographic_title": "Як психологічно підтримати постраждалих від війни?",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/signal-2022-09-26-153257_003.jpeg"
+        "video_id": "2",
+        "video_title": "Фейки вбивають. Випуск №2",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F448568197135834%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "3",
-        "infographic_title": "Як недоцільно спілкуватися із постраждалими від війни?",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/signal-2022-09-26-153257_002.jpeg"
+        "video_id": "3",
+        "video_title": "Фейки вбивають. Випуск №3",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F1225609998274728%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "4",
-        "infographic_title": "Наслідки у разі аварії на ЗАЕС",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/ZAES-1-1.png"
+        "video_id": "4",
+        "video_title": "Фейки вбивають. Випуск №4",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F432505645600907%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "5",
-        "infographic_title": "Алгоритм дій у разі радіоактивної загрози",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/ZAES-3-1.png"
+        "video_id": "5",
+        "video_title": "Фейки вбивають. Випуск №5",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F5299345763489904%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "6",
-        "infographic_title": "Фейки про ЗАЕС",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/signal-2022-09-26-154042_002-1.jpeg"
+        "video_id": "6",
+        "video_title": "Фейки вбивають. Випуск №6",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F1469430120242251%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "7",
-        "infographic_title": "Фейки про ЗАЕС",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/signal-2022-09-26-154042_003.jpeg"
+        "video_id": "7",
+        "video_title": "Фейки вбивають. Випуск №7",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F5644179615646587%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "8",
-        "infographic_title": "Фейки про ЗАЕС",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/signal-2022-09-26-154042_004.jpeg"
+        "video_id": "8",
+        "video_title": "Фейки вбивають. Випуск №8",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F2010313459169052%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "9",
-        "infographic_title": "Фейки про ЗАЕС",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/signal-2022-09-26-154042_005.jpeg"
+        "video_id": "9",
+        "video_title": "Фейки вбивають. Випуск №9",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F609858383974126%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "10",
-        "infographic_title": "Фейки про ЗАЕС",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/signal-2022-09-26-154042_006.jpeg"
+        "video_id": "10",
+        "video_title": "Фейки вбивають. Випуск №10",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F383936327261246%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "11",
-        "infographic_title": "російські окупанти вбивають медицину в Україні",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/signal-2022-09-26-153340_002.jpeg"
+        "video_id": "11",
+        "video_title": "Фейки вбивають. Випуск №11",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F1506620836519771%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "12",
-        "infographic_title": "Чому чорна трансплантологія — це міф?",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/CHorni-transplantology.png"
+        "video_id": "12",
+        "video_title": "Фейки вбивають. Випуск №12",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F590081966172895%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "13",
-        "infographic_title": "Міфи про мавпячу віспу",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/signal-2022-09-26-153429_003.jpeg"
+        "video_id": "13",
+        "video_title": "Фейки вбивають. Спецвипуск",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F625606498933890%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "14",
-        "infographic_title": "Профілактика мавпячої віспи",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/signal-2022-09-26-153429_002.jpeg"
+        "video_id": "14",
+        "video_title": "Фейки вбивають. Випуск №14",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F1208334123058787%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "15",
-        "infographic_title": "Україні не загрожує голод!",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/signal-2022-09-26-153443_002.jpeg"
+        "video_id": "15",
+        "video_title": "Фейки вбивають. Випуск №15",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F5486291408123611%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "16",
-        "infographic_title": "В Україні немає дефіциту олії!",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/signal-2022-09-26-153455_002.jpeg"
+        "video_id": "16",
+        "video_title": "Фейки вбивають. Випуск №16",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F2485725614919708%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "17",
-        "infographic_title": "Здорове серце",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/Oglyad-naratyviv-6.jpg"
+        "video_id": "17",
+        "video_title": "Фейки про хід медичної реформи в Україні",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F3325173757704494%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "18",
-        "infographic_title": "Україна відновлюється!",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/09/Doslidy-3-768x768.jpg"
+        "video_id": "18",
+        "video_title": "Фейки про донорство крові",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F895847071858042%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "19",
-        "infographic_title": "Як ваша вакцинація від COVID-19 рятує інших?",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/11/voxcheck-ukr.jpg"
+        "video_id": "19",
+        "video_title": "Нові фейки про біолабораторії в Україні",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F556336506383444%2F&show_text=false&width=560&t=0"
     },
     {
-        "infographic_id": "20",
-        "infographic_title": "Вакцинація дітей від COVID-19",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/11/voxcheck-ukr-1-768x768.jpg"
-    },
-    {
-        "infographic_id": "21",
-        "infographic_title": "Брехня рф про гуманітарну ситуацію в Україні",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/11/image1-1-1-1024x1024.png"
-    },
-    {
-        "infographic_id": "22",
-        "infographic_title": "Брехня рф про гуманітарну ситуацію в Україні",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/11/image2-1024x1024.png"
-    },
-    {
-        "infographic_id": "23",
-        "infographic_title": "Брехня рф про гуманітарну ситуацію в Україні",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/11/image4-1024x1024.png"
-    },
-    {
-        "infographic_id": "24",
-        "infographic_title": "Міф про \"біолабораторії США\" в Україні",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/11/mif-laboratorii-.png"
-    },
-    {
-        "infographic_id": "25",
-        "infographic_title": "Що насправді США зробили у рамках Програми зі зменшення біологічної загрози?",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/11/mif-ssha.png"
-    },
-    {
-        "infographic_id": "26",
-        "infographic_title": "(Без)оплатна медицина в Україні",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/12/medytsyna-5-768x768.jpeg"
-    },
-    {
-        "infographic_id": "27",
-        "infographic_title": "Доступні ліки",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/12/medytsyna-6-768x768.png"
-    },
-    {
-        "infographic_id": "28",
-        "infographic_title": "Як стати донором крові в Україні?",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/12/image5-1-768x768.png"
-    },
-    {
-        "infographic_id": "29",
-        "infographic_title": "Що має бути в Пункті незламності?",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/12/image2-1-768x768.png"
-    },
-    {
-        "infographic_id": "30",
-        "infographic_title": "Ефективність канабіноїдів",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/12/legalizatsiya-medychnogo-kanabisu-1-768x768.jpg"
-    },
-    {
-        "infographic_id": "31",
-        "infographic_title": "Скільки українців потребує медичного канабісу?",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2022/12/legalizatsiya-medychnogo-kanabisu-2.png"
-    },
-    {
-        "infographic_id": "32",
-        "infographic_title": "Заява на донорство",
-        "infographic_link": "https://voxukraine.org/wp-content/uploads/2023/01/image2-768x768.jpg"
+        "video_id": "20",
+        "video_title": "Фейки про медичний канабіс та тест на москаля",
+        "video_link": "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FVoxCheck%2Fvideos%2F854813255639110%2F&show_text=false&width=560&t=0"
     }
 ]
